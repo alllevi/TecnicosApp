49bdfd19a52b0ff505389d16ba61f41226c18574
allv7 <yedo_7@hotmail.com>
2016-05-13 21:02:40 +0200

    Añadida funcionalidad a RegistroActivity

--- app/src/main/java/com/manomanitas/tecnicosapp/RegistroActivity.java
+++ app/src/main/java/com/manomanitas/tecnicosapp/RegistroActivity.java
@@ -3,21 +3,10 @@ package com.manomanitas.tecnicosapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -26,32 +15,14 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static android.Manifest.permission.READ_CONTACTS;
+public class RegistroActivity extends AppCompatActivity {
 
-/**
- * A login screen that offers login via email/password.
- */
-public class RegistroActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -60,18 +31,73 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
+    private EditText mNombreView;
+    private EditText mDniView;
+    private EditText mTelefonoView;
+    private EditText mCodigoPostalView;
+    private EditText mRadioView;
+
+    private Spinner spinnerProvincia;
+    private Spinner spinnerMunicipio;
+
+    private CheckBox cElectricidad;
+    private CheckBox cFontaneria;
+    private CheckBox cCerrajero;
+    private CheckBox cVideo;
+    private CheckBox cAntenista;
+    private CheckBox cTelefonillo;
+    private CheckBox cClima;
+    private CheckBox cCalentador;
+    private CheckBox cCalefaccion;
+    private CheckBox cAlarma;
+    private CheckBox cElectro;
+
+    private CheckBox cAvisos;
+
     private View mProgressView;
     private View mLoginFormView;
+
+    /*TAREAS
+    * Datos reales spinners
+    * Seleccion de spinners
+    * Al menos un checkBox
+    * Paso de datos
+    * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
-
         mPasswordView = (EditText) findViewById(R.id.password);
+        mNombreView = (EditText) findViewById(R.id.nombre_registro);
+        mDniView = (EditText) findViewById(R.id.dni_registro);
+        mTelefonoView = (EditText) findViewById(R.id.telefono_registro);
+        mCodigoPostalView = (EditText) findViewById(R.id.codigoPostal_registro);
+        mRadioView = (EditText) findViewById(R.id.radio_registro);
+
+        spinnerProvincia = (Spinner) findViewById(R.id.provincia_registro);
+        spinnerMunicipio = (Spinner) findViewById(R.id.municipio_registro);
+
+        cElectricidad = (CheckBox) findViewById(R.id.electricidad_checkBox);
+        cFontaneria = (CheckBox) findViewById(R.id.fontaneria_checkBox);
+        cCerrajero = (CheckBox) findViewById(R.id.cerrajeria_checkBox);
+        cVideo = (CheckBox) findViewById(R.id.video_checkBox);
+        cAntenista = (CheckBox) findViewById(R.id.antenista_checkBox);
+        cTelefonillo = (CheckBox) findViewById(R.id.telefono_checkBox);
+        cClima = (CheckBox) findViewById(R.id.clima_checkBox);
+        cCalentador = (CheckBox) findViewById(R.id.calentador_checkBox);
+        cCalefaccion = (CheckBox) findViewById(R.id.calefaccion_checkBox);
+        cAlarma = (CheckBox) findViewById(R.id.alarma_checkBox);
+        cElectro = (CheckBox) findViewById(R.id.electro_checkBox);
+
+        cAvisos = (CheckBox) findViewById(R.id.avisos_checkBox);
+
+        mLoginFormView = findViewById(R.id.registro_form);
+        mProgressView = findViewById(R.id.registro_progress);
+
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -83,7 +109,7 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.registro_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,11 +117,6 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
-
-        //--------------------
-        Spinner spinnerProvincia = (Spinner) findViewById(R.id.provincia_registro);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapterP = ArrayAdapter.createFromResource(this,
                 R.array.planets_array, android.R.layout.simple_spinner_item);
@@ -104,7 +125,7 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
 // Apply the adapter to the spinner
         spinnerProvincia.setAdapter(adapterP);
 
-        Spinner spinnerMunicipio = (Spinner) findViewById(R.id.municipio_registro);
+
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapterM = ArrayAdapter.createFromResource(this,
                 R.array.planets_array, android.R.layout.simple_spinner_item);
@@ -114,52 +135,8 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
         spinnerMunicipio.setAdapter(adapterM);
     }
 
-    private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
-        }
-
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
     /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
-
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
+     * Attempts to register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
@@ -171,20 +148,25 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
+        mNombreView.setError(null);
+        mDniView.setError(null);
+        mTelefonoView.setError(null);
+        mCodigoPostalView.setError(null);
+        mRadioView.setError(null);
+
 
         // Store values at the time of the login attempt.
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
+        String nombre = mNombreView.getText().toString();
+        String dni = mDniView.getText().toString();
+        String telefono = mTelefonoView.getText().toString();
+        String codigoPostal = mCodigoPostalView.getText().toString();
+        String radio = mRadioView.getText().toString();
+        //faltan elementos
 
         boolean cancel = false;
         View focusView = null;
-
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
@@ -197,6 +179,61 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
             cancel = true;
         }
 
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password_signIn));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(nombre)) {
+            mNombreView.setError(getString(R.string.error_invalid_password_signIn));
+            focusView = mNombreView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(dni)) {
+            mDniView.setError(getString(R.string.error_field_required));
+            focusView = mDniView;
+            cancel = true;
+        } else if (!isDniValid(dni)) {
+            mDniView.setError(getString(R.string.error_invalid_email));
+            focusView = mDniView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(telefono)) {
+            mTelefonoView.setError(getString(R.string.error_invalid_password_signIn));
+            focusView = mTelefonoView;
+            cancel = true;
+        } else if (!isTelefonoValid(telefono)) {
+            mTelefonoView.setError(getString(R.string.error_invalid_password_signIn));
+            focusView = mTelefonoView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(codigoPostal)) {
+            mCodigoPostalView.setError(getString(R.string.error_invalid_password_signIn));
+            focusView = mCodigoPostalView;
+            cancel = true;
+        } else if (!isCodigoPostalValid(codigoPostal)) {
+            mCodigoPostalView.setError(getString(R.string.error_invalid_password_signIn));
+            focusView = mCodigoPostalView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(radio)) {
+            mRadioView.setError(getString(R.string.error_invalid_password_signIn));
+            focusView = mRadioView;
+            cancel = true;
+        } else if (!isRadioValid(radio)) {
+            mRadioView.setError(getString(R.string.error_invalid_password_signIn));
+            focusView = mRadioView;
+            cancel = true;
+        }
+
+        //spineers y al menos un check
+
+
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
@@ -205,19 +242,30 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(email, password, nombre, dni, telefono, codigoPostal, radio);
             mAuthTask.execute((Void) null);
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+        return true;
+        //return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+    private boolean isDniValid(String dni) {
+        return true;
+    }
+
+    private boolean isTelefonoValid(String telefono) {
+        return true;
+    }
+
+    private boolean isCodigoPostalValid(String cp) {
+        return true;
+    }
+
+    private boolean isRadioValid(String radio) {
+        return true;
     }
 
     /**
@@ -256,72 +304,28 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
         }
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(RegistroActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
     /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
+     * Represents an asynchronous registration task
      */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
         private final String mPassword;
+        private final String mNombre;
+        private final String mDni;
+        private final String mTelefono;
+        private final String mCodigoPostal;
+        private final String mRadio;
 
-        UserLoginTask(String email, String password) {
+
+        UserLoginTask(String email, String password, String nombre, String dni, String telefono, String cp, String radio) {
             mEmail = email;
             mPassword = password;
+            mNombre = nombre;
+            mDni = dni;
+            mTelefono = telefono;
+            mCodigoPostal = cp;
+            mRadio = radio;
         }
 
         @Override
@@ -333,14 +337,6 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
             }
 
             // TODO: register the new account here.
@@ -355,8 +351,7 @@ public class RegistroActivity extends AppCompatActivity implements LoaderCallbac
             if (success) {
                 finish();
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+
             }
         }
 
